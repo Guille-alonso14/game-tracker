12a86b2f73d23064559f792c3a2f194b95087db8
Guillermo Alonso <guillermo.alonso@alu.ceacfp.es>
2026-04-27 12:01:31 +0200

    fix: remove dotenv from supabase config

--- server/src/config/supabase.ts
+++ server/src/config/supabase.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { config } from 'dotenv'
-
-config()
 
 const supabaseUrl = process.env.SUPABASE_URL ?? ''
 const supabaseKey = process.env.SUPABASE_ANON_KEY ?? ''
